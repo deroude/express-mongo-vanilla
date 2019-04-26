c88f2a5b26ba436f3cba59c2d134fce027293ba5
Gabriel <deroude@gmail.com>
2019-04-26 12:23:55 +0300

    first commit

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM node:10 AS dependencies
+ADD package.json .
+RUN npm install
+
+FROM dependencies
+ADD index.js .
+EXPOSE 3000
+ENTRYPOINT [ "node", "index.js" ]
---
+++ config.js
@@ -0,0 +1,3 @@
+// export const url = 'mongodb://root:example@mongo:27017/example';
+export const url = 'mongodb://root:example@localhost:27017/example';
+export const serverport = 3000;
